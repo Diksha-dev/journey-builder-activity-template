33f2e25fd35357d7c44babb8a14845f9d19f7871
Diksha-dev <68223222+Diksha-dev@users.noreply.github.com>
2020-07-17 13:57:20 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -66,11 +66,10 @@ define([
     }
 
     function save() {
-        var postcardURLValue = $('#postcard-url').val();
-        var postcardTextValue = $('#postcard-text').val();
-
+       
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens
+            
            
         }];
         
